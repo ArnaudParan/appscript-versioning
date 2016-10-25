32858dd596c05b199235be6a621f8e9d8b584fc6
Arnaud Paran <paran.arnaud@gmail.com>
2016-10-25 17:12:11 +0200

    first requests to get the list of appscript files in the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,52 @@
 var gulp = require('gulp');
+var request = require('request');
+var fs = require('fs');
 
 gulp.task('default', function() {
-    // place code for your default task here
+  readConfig(function(configs) {
+    getAppscriptList(configs, function(appscriptList) {
+      console.log(appscriptList);
+    });
+  });
 });
+
+/* Reads the config.json file and parses the result as a json dict, then
+ * calls callback with that json dict
+ */
+function readConfig(callback) {
+  fs.readFile('config.json', 'utf8', function(error, file) {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        throw 'Error, file "config.json" does not exist';
+      }
+      else {
+        throw error;
+      }
+    }
+    var configs = JSON.parse(file);
+    callback(configs);
+  });
+}
+
+function getAppscriptList(configs, callback) {
+  var options = {
+    url: 'https://script.google.com/feeds/download/export?id=' + configs.appscriptID + '&format=json',
+    headers: {
+      'Authorization': 'Bearer ' + configs.accessToken
+    }
+  };
+  request(options, function(error, response, body) {
+    if (error) {
+      throw error;
+    }
+    else if (response.statusCode !== 200) {
+      throw 'Wrong response code ' + response.statusCode + '\n' + body;
+    }
+    else if (!error && response.statusCode == 200) {
+      var appscriptList = JSON.parse(body);
+      if (callback) {
+        callback(appscriptList);
+      }
+    }
+  });
+}
